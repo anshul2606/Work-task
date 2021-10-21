97a64e84645beab773a6d54a99c1a5ac81d6090e
anshul2606 <anshul93.jain@gmail.com>
2021-10-21 12:31:23 +0530

    chnages

--- mylibrary/src/main/java/com/empire/mylibrary/Connect.java
+++ mylibrary/src/main/java/com/empire/mylibrary/Connect.java
@@ -3,7 +3,19 @@ package com.empire.mylibrary;
 import android.content.Context;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class Connect {
+
+    public String resp = "";
 
     public void toast(Context mContext, String msg) {
         Toast.makeText(mContext, msg, Toast.LENGTH_SHORT).show();
@@ -17,6 +29,38 @@ public class Connect {
             dialog.cancel();
         });
         builder.show();
+    }
+
+    public String getDashboardData_R(Context mContext, String user_id, String route, String version_code, String token) {
+
+        Api api = ApiClient.getClient().create(Api.class);
+        Map<String, String> params = new HashMap<>();
+
+        params.put("user_id", user_id);
+        params.put("route", route);
+        params.put("ER_app_version", version_code);
+
+        api.getDashboardData(params, "Bearer " + token).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+                if (response.isSuccessful()) {
+                    try {
+                        String res = response.body().string();
+
+                        resp = res;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                resp = "";
+            }
+        });
+        return resp;
     }
 
 }
